5111a93014a5a57993dfc98f243f2846b141c4f8
Parker Nalchajian <parker.nalchajian@gmail.com>
2019-06-24 19:30:41 -0700

    added reactive elements and transitions through CSSTransitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppNavbar from './components/AppNavbar';
+import CharacterList from './components/CharacterList';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <CharacterList />
     </div>
   );
 }
