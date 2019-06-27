852be7c4dc9a36c45f076b961f9cb777ef53416d
Parker Nalchajian <parker.nalchajian@gmail.com>
2019-06-27 16:04:41 -0700

    run loadUser on application start

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import CharacterList from './components/CharacterList';
 import CharacterModal from './components/CharacterModal';
@@ -7,11 +7,14 @@ import { Container } from 'reactstrap';
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { loadUser } from "./actions/authActions";
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
+  store.dispatch(loadUser());
+
   return (
     <Provider store={store}>
       <div className="App">
