821ba1e303bebbaf7770e535a5270aa7b8788180
Parker Nalchajian <parker.nalchajian@gmail.com>
2019-06-27 14:54:28 -0700

    added reducers for auth and errors

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from 'redux';
 import characterReducer from './characterReducer';
+import errorReducer from './errorReducer';
+import authReducer from './authReducer'
 
 export default combineReducers({
-    character: characterReducer
+    character: characterReducer,
+    error: errorReducer,
+    auth: authReducer
 })
