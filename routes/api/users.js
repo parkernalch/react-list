c200e98be8e350b373322131266c9f7886d83570
Parker Nalchajian <parker.nalchajian@gmail.com>
2019-06-27 11:45:35 -0700

    user registration path now correctly adds user to mongodb

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 // User Model
@@ -9,7 +10,42 @@ const User = require('../../models/User');
 // @desc    register new user
 // @access  Public
 router.post('/', (req, res) => {
-    res.send('register');
+    const {name, email, password } = req.body;
+
+    // simple validation
+    if (!name || !email || !password ) {
+        return res.status(400).json({message: 'Please enter all fields'});
+    }
+
+    // Check for existing user
+    User.findOne({ email: email })
+        .then(user => {
+            if(user) return res.status(400).json({ message: "User already exists" });
+
+            const newUser = new User({
+                name,
+                email,
+                password
+            });
+
+            // Create Salt  & hash password
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) =>{
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then(user => {
+                            res.json({
+                                user: {
+                                    id: user.id,
+                                    name: user.name,
+                                    email: user.email
+                                }
+                            });
+                        });
+                });
+            });
+        });
 });
 
 module.exports = router;
